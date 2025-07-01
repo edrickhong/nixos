bf4fd4a54adfd6628332c66bda30af693b5d6895
ehzx <edrickhong@gmail.com>
2025-07-01 14:11:03 -0400

    change stateVersion. risky but silences home-manager switch --show-trace. (this is for tracking breaking changes, try not to change)

--- home.nix
+++ home.nix
@@ -13,8 +13,8 @@
 # You should not change this value, even if you update Home Manager. If you do
 # want to update the value, then make sure to first check the Home Manager
 # release notes.
-	home.stateVersion = "24.11"; # Please read the comment before changing.
-		nixpkgs.config.allowUnfree = true;
+	home.stateVersion = "25.05"; # Please read the comment before changing.
+	nixpkgs.config.allowUnfree = true;
 
 
 # The home.packages option allows you to install Nix packages into your
