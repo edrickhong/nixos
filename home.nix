459657f635ecb00eef043df83dded6db54880847
ehzx <edrickhong@gmail.com>
2025-07-01 11:29:13 -0400

    enabling fonts

--- home.nix
+++ home.nix
@@ -70,6 +70,9 @@
 			]);
 
 
+	fonts.fontconfig.enable = true;
+
+
 # Home Manager is pretty good at managing dotfiles. The primary way to manage
 # plain files is through 'home.file'.
 	home.file = {
